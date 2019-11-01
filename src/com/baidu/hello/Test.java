1ee531f3082bbf4ee264cedce0945c96cc6f20a7
vaynexiao <576583212@qq.com>
2019-10-31 18:23:46 -0700

    222222222222222

--- src/com/baidu/hello/Test.java
+++ src/com/baidu/hello/Test.java
@@ -3,5 +3,6 @@ package com.baidu.hello;
 public class Test {
     public static void main(String[] args) {
         System.out.println("532455");
+        System.out.println("2222222222222222222222222222222222");
     }
 }
